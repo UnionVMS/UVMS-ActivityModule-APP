d80e71eb9278320d4ebbb39002b638be491fab6a
emkoviand <emkoviand@mycompany.com>
2016-05-04 13:57:12 +0000

    [andi.kovi]
    Some entities.
    Changed po.xml for the generation of classes from xsd.
    Added the needed xsd's.
    
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Activity/APP@11932 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/mdr/dao/AbstractMdrDao.java
+++ service/src/main/java/eu/europa/ec/fisheries/mdr/dao/AbstractMdrDao.java
@@ -4,5 +4,27 @@ import eu.europa.ec.fisheries.mdr.domain.MasterDataRegistry;
 import eu.europa.ec.fisheries.uvms.service.AbstractDAO;
 
 public abstract class AbstractMdrDao <E extends MasterDataRegistry> extends AbstractDAO<E> {
-
+	
+    /* public void bulkInsert(Map<String, List<Property>> features) throws ServiceException {
+        StatelessSession session = (getEntityManager().unwrap(Session.class)).getSessionFactory().openStatelessSession();
+        Transaction tx = session.beginTransaction();
+        try {
+            Query query = session.getNamedQuery(getDisableAreaNamedQuery());
+            query.executeUpdate();
+            for (List<Property> properties : features.values()) {
+                Map<String, Object> values = BaseAreaEntity.createAttributesMap(properties);
+                session.insert(createEntity(values));
+            }
+            log.debug("Commit transaction");
+            tx.commit();
+        }
+        catch (ServiceException e){
+            tx.rollback();
+            throw new ServiceException("Rollback transaction", e);
+        }
+        finally {
+            log.debug("Closing session");
+            session.close();
+        }
+    } */
 }
