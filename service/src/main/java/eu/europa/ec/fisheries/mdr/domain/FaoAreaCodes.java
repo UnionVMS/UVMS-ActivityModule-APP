003bc1213fe7a2602160332b68690d5c0238c6b4
emkoviand <emkoviand@mycompany.com>
2016-05-04 14:12:15 +0000

    [andi.kovi]
    Some entities.
    Changed po.xml for the generation of classes from xsd.
    Added the needed xsd's.
    
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Activity/APP@11939 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ service/src/main/java/eu/europa/ec/fisheries/mdr/domain/FaoAreaCodes.java
@@ -0,0 +1,87 @@
+package eu.europa.ec.fisheries.mdr.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * @author kovian
+ *
+ */
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "fao_area_codes")
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class FaoAreaCodes extends MasterDataRegistry {
+
+	@Column(name = "area")
+	private String area;
+	
+	@Column(name = "subarea")
+	private String subarea;
+	
+	@Column(name = "division")
+	private String division;
+	
+	@Column(name = "subdivision")
+	private String subdivision;
+	
+	@Column(name = "unit")
+	private String unit;
+	
+	@Column(name = "description")
+	private String description;
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getSubarea() {
+		return subarea;
+	}
+
+	public void setSubarea(String subarea) {
+		this.subarea = subarea;
+	}
+
+	public String getDivision() {
+		return division;
+	}
+
+	public void setDivision(String division) {
+		this.division = division;
+	}
+
+	public String getSubdivision() {
+		return subdivision;
+	}
+
+	public void setSubdivision(String subdivision) {
+		this.subdivision = subdivision;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
