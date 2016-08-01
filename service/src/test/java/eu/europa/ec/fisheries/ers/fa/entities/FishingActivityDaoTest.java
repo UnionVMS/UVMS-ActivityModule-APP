80d8d585a9d8212e152399c38d7d71d9f5dad47b
emsanerac <emsanerac@mycompany.com>
2016-08-01 09:14:30 +0000

    Test class added for FishingActivityDao
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Activity/APP@13410 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/test/java/eu/europa/ec/fisheries/ers/fa/entities/FishingActivityDaoTest.java
+++ service/src/test/java/eu/europa/ec/fisheries/ers/fa/entities/FishingActivityDaoTest.java
@@ -14,10 +14,15 @@ import com.ninja_squad.dbsetup.DbSetup;
 import com.ninja_squad.dbsetup.destination.DataSourceDestination;
 import com.ninja_squad.dbsetup.operation.Operation;
 import eu.europa.ec.fisheries.ers.fa.dao.FishingActivityDao;
+import eu.europa.ec.fisheries.ers.service.search.*;
 import lombok.SneakyThrows;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static com.ninja_squad.dbsetup.Operations.sequenceOf;
 import static junit.framework.TestCase.assertNotNull;
@@ -50,26 +55,42 @@ private FishingActivityDao dao=new FishingActivityDao(em);
     public void testSearchEntityByQuery() throws Exception {
 
         dbSetupTracker.skipNextLaunch();
-      //  FishingActivityQuery query = new FishingActivityQuery();
-       /* ListCriteria listCriteria = new ListCriteria();
-        listCriteria.setKey(SearchKey.typeCode);
-        listCriteria.setValue("FISHING_OPERATION");
+        FishingActivityQuery query = new FishingActivityQuery();
+        List<ListCriteria> list = new ArrayList<ListCriteria>();
 
-        ListCriteria listCriteria1 = new ListCriteria();
-        listCriteria1.setKey(SearchKey.reasonCodeListId);
-        listCriteria1.setValue("LAN");
 
-        List<ListCriteria> searchCriteria = new ArrayList<ListCriteria>();
-        searchCriteria.add(listCriteria);
-        query.setSearchCriteria(searchCriteria);
+        list.add(new ListCriteria(Filters.VESSEL_IDENTIFIES, Arrays.asList(new SearchValue(FilterMap.VESSEL_IDENTITY_NAME,"vesselGroup1") )));
+        list.add(new ListCriteria(Filters.PURPOSE, Arrays.asList(new SearchValue(FilterMap.PURPOSE_CODE,"PURPOSE") )));
+        list.add(new ListCriteria(Filters.MASTER, Arrays.asList(new SearchValue(FilterMap.CONTACT_PERSON_NAME,"MARK") )));
 
-        query.setSortKey(new SortKey(SearchKey.occurence, SortOrder.ASC));
+        list.add(new ListCriteria(Filters.REPORT_TYPE, Arrays.asList(new SearchValue(FilterMap.REPORT_TYPE_CODE,"DOC_TYPE_1") )));
 
-        query.setPagination( new Pagination(1,2));*/
-      //  List<FishingActivityEntity> finishingActivityList=  dao.getFishingActivityListByQuery(query);
+        list.add(new ListCriteria(Filters.REPORT_TYPE, Arrays.asList(new SearchValue(FilterMap.REPORT_TYPE_CODE,"DOC_TYPE_1") )));
+        list.add(new ListCriteria(Filters.ACTIVITY_TYPE, Arrays.asList(new SearchValue(FilterMap.ACTIVITY_TYPE_CODE,"FISHING_OPERATION") )));
+        list.add(new ListCriteria(Filters.AREAS, Arrays.asList(new SearchValue(FilterMap.AREA_ID,"FLUX_LOCATION_ID") )));
+        list.add(new ListCriteria(Filters.GEAR, Arrays.asList(new SearchValue(FilterMap.FISHING_GEAR,"GEAR_TYPE") )));
+        list.add(new ListCriteria(Filters.SPECIES, Arrays.asList(new SearchValue(FilterMap.SPECIES_CODE,"beagle1") )));
+        list.add(new ListCriteria(Filters.QUNTITIES, Arrays.asList(new SearchValue(FilterMap.UNIT_QUANTITY,"111") )));
+        list.add(new ListCriteria(Filters.PERIOD, Arrays.asList(new SearchValue(FilterMap.OCCURENCE_START_DATE,"2014-05-27 07:47:31"),new SearchValue(FilterMap.OCCURENCE_END_DATE,"2015-05-27 07:47:31") )));
+        list.add(new ListCriteria(Filters.FROM, Arrays.asList(new SearchValue(FilterMap.FROM_NAME,"flux1") )));
 
-     //   assertNotNull(finishingActivityList);
+        query.setSearchCriteria(list);
+        // query.setPagination( new Pagination(1,2));
+
+        List<FishingActivityEntity> finishingActivityList=dao.getFishingActivityListByQuery(query);
+
+        assertNotNull(finishingActivityList);
 
     }
 
+    @Test
+    @SneakyThrows
+    public void testGetFishingActivityList() throws Exception {
+
+        dbSetupTracker.skipNextLaunch();
+
+        List<FishingActivityEntity> finishingActivityList=dao.getFishingActivityList();
+        assertNotNull(finishingActivityList);
+
+    }
 }
