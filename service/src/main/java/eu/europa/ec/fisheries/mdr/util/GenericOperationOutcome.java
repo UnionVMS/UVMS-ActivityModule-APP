bae36ba3bb4084ccb826cf4a131e2722a7948aa6
emkoviand <emkoviand@mycompany.com>
2016-09-29 15:54:07 +0000

    Code quality, refactoring
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Activity/APP@13869 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/mdr/util/GenericOperationOutcome.java
+++ service/src/main/java/eu/europa/ec/fisheries/mdr/util/GenericOperationOutcome.java
@@ -21,6 +21,8 @@ public class GenericOperationOutcome {
     private int counter                   = 0;
     private Object includedObject         = null;
 
+    public GenericOperationOutcome(){}
+
     public GenericOperationOutcome(OperationOutcome status){
         this.status = status;
     }
