a075f6fc0bc4ae28b6bd520f34bce38823a139e5
emkoviand <emkoviand@mycompany.com>
2016-09-29 13:02:19 +0000

    Corrected no anymore working test due to model change
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Activity/APP@13865 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/test/java/eu/europa/ec/fisheries/ers/service/bean/ActivityServiceBeanTest.java
+++ service/src/test/java/eu/europa/ec/fisheries/ers/service/bean/ActivityServiceBeanTest.java
@@ -15,23 +15,16 @@ package eu.europa.ec.fisheries.ers.service.bean;
 
 import eu.europa.ec.fisheries.ers.fa.dao.*;
 import eu.europa.ec.fisheries.ers.fa.entities.FaReportDocumentEntity;
-import eu.europa.ec.fisheries.ers.fa.utils.FaReportSourceEnum;
-import eu.europa.ec.fisheries.ers.message.exception.ActivityMessageException;
 import eu.europa.ec.fisheries.ers.message.producer.bean.ActivityMessageProducerBean;
-import eu.europa.ec.fisheries.ers.service.mapper.FaReportDocumentMapper;
 import eu.europa.ec.fisheries.ers.service.search.Filters;
 import eu.europa.ec.fisheries.ers.service.search.FishingActivityQuery;
-import eu.europa.ec.fisheries.ers.service.search.ListCriteria;
 import eu.europa.ec.fisheries.ers.service.search.Pagination;
-import eu.europa.ec.fisheries.ers.service.util.ActivityDataUtil;
 import eu.europa.ec.fisheries.ers.service.util.MapperUtil;
+import eu.europa.ec.fisheries.schema.audit.search.v1.ListCriteria;
 import eu.europa.ec.fisheries.uvms.activity.model.dto.FilterFishingActivityReportResultDTO;
 import eu.europa.ec.fisheries.uvms.activity.model.dto.fareport.FaReportCorrectionDTO;
-import eu.europa.ec.fisheries.uvms.activity.model.dto.fishingtrip.*;
-import eu.europa.ec.fisheries.uvms.activity.model.exception.ModelMarshallException;
 import eu.europa.ec.fisheries.uvms.activity.model.mapper.JAXBMarshaller;
 import eu.europa.ec.fisheries.uvms.exception.ServiceException;
-import eu.europa.ec.fisheries.uvms.message.MessageException;
 import lombok.SneakyThrows;
 import org.junit.Rule;
 import org.junit.Test;
@@ -40,10 +33,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
-import un.unece.uncefact.data.standard.reusableaggregatebusinessinformationentity._18.FAReportDocument;
 
-import javax.jms.JMSException;
-import javax.jms.TextMessage;
 import javax.persistence.EntityManager;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -52,7 +42,6 @@ import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
@@ -125,68 +114,6 @@ public class ActivityServiceBeanTest {
 
     @Test
     @SneakyThrows
-    public void testGetVesselDetailsAndContactPartiesForFishingTrip() throws ServiceException {
-
-        when(fishingTripDao.fetchVesselTransportDetailsForFishingTrip("NOR-TRP-20160517234053706")).thenReturn(MapperUtil.getFishingTripEntityWithContactParties());
-
-        VesselDetailsTripDTO vesselDetailsTripDTO = fishingTripService.getVesselDetailsForFishingTrip("NOR-TRP-20160517234053706");
-
-        assertNotNull(vesselDetailsTripDTO);
-        assertEquals(2, vesselDetailsTripDTO.getContactPersons().size());
-        assertEquals("vesselGroup1", vesselDetailsTripDTO.getName());
-    }
-
-    @Test
-    @SneakyThrows
-    public void testEnrichVesselDetailsAndContactPartiesForFishingTrip() throws ServiceException, ModelMarshallException, ActivityMessageException, MessageException, JMSException {
-
-        String response = JAXBMarshaller.marshallJaxBObjectToString(ActivityDataUtil.getListAssetResponse());
-        TextMessage mockTextMessage = mock(TextMessage.class);
-
-        when(fishingTripDao.fetchVesselTransportDetailsForFishingTrip("NOR-TRP-20160517234053706")).thenReturn(MapperUtil.getFishingTripEntityWithContactParties());
-        when(activityProducer.sendAssetsModuleSynchronousMessage("")).thenReturn("0101");
-        when(activityConsumer.getMessage(null, TextMessage.class)).thenReturn(mockTextMessage);
-        when(mockTextMessage.getText()).thenReturn(response);
-
-        VesselDetailsTripDTO vesselDetailsTripDTO = fishingTripService.getVesselDetailsForFishingTrip("NOR-TRP-20160517234053706");
-
-        assertNotNull(vesselDetailsTripDTO);
-
-        // Checking that the VesselDetailsTripDTO has been enriched;
-        assertEquals("UPDATED_IRCS", vesselDetailsTripDTO.getIrcs());
-        assertEquals(true, vesselDetailsTripDTO.isIrcsEnriched());
-
-        assertEquals("UPDATED_IMO", vesselDetailsTripDTO.getUvi());
-        assertEquals(true, vesselDetailsTripDTO.isUviEnriched());
-
-        assertEquals(false, vesselDetailsTripDTO.isExMarkEnriched());
-        assertEquals(false, vesselDetailsTripDTO.isFlagStateEnriched());
-    }
-
-
-    @Test
-    @SneakyThrows
-    public void getFishingActivityReportAndRelatedDataForFishingTrip() throws ServiceException {
-
-        when(fishingActivityDao.getFishingActivityListForFishingTrip("NOR-TRP-20160517234053706",null)).thenReturn(MapperUtil.getFishingActivityEntityList());
-
-        List<ReportDTO> reportDTOList = new ArrayList<>();
-        Map<String, FishingActivityTypeDTO > summary = new HashMap<>();
-        MessageCountDTO messagesCount = new MessageCountDTO();
-        //Trigger
-        fishingTripService.getFishingActivityReportAndRelatedDataForFishingTrip("NOR-TRP-20160517234053706",reportDTOList,summary,messagesCount);
-
-        Mockito.verify(fishingActivityDao, Mockito.times(1)).getFishingActivityListForFishingTrip(Mockito.any(String.class),Mockito.any(Pagination.class));
-        //Verify
-
-        assertEquals(3, summary.size());
-        assertEquals(3, reportDTOList.size());
-        assertEquals(3, messagesCount.getNoOfDeclarations());
-
-    }
-
-    @Test
-    @SneakyThrows
     public void getFishingActivityListByQuery() throws ServiceException {
 
         FishingActivityQuery query = new FishingActivityQuery();
@@ -217,10 +144,4 @@ public class ActivityServiceBeanTest {
 
     }
 
-    private List<FaReportDocumentEntity> getMockedFishingActivityReportEntities() {
-        List<FaReportDocumentEntity> faReportDocumentEntities = new ArrayList<>();
-        FAReportDocument faReportDocument = MapperUtil.getFaReportDocument();
-        faReportDocumentEntities.add(FaReportDocumentMapper.INSTANCE.mapToFAReportDocumentEntity(faReportDocument, new FaReportDocumentEntity(), FaReportSourceEnum.MANUAL));
-        return faReportDocumentEntities;
-    }
 }
