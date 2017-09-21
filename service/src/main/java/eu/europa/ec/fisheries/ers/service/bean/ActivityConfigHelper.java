88d25e086bc424b8007f12e9670c010c46425639
James Sörling <james.p.sorling@consid.se>
2017-09-21 12:32:33 +0200

    update models, gregoriancalender -> date. Add metrics and uvms-config.

---
+++ service/src/main/java/eu/europa/ec/fisheries/ers/service/bean/ActivityConfigHelper.java
@@ -0,0 +1,50 @@
+/*
+﻿Developed with the contribution of the European Commission - Directorate General for Maritime Affairs and Fisheries
+© European Union, 2015-2016.
+
+This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can
+redistribute it and/or modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or any later version. The IFDM Suite is distributed in
+the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
+copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.europa.ec.fisheries.ers.service.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import eu.europa.ec.fisheries.ers.constant.ParameterKey;
+import eu.europa.ec.fisheries.uvms.config.constants.ConfigHelper;
+
+@Stateless
+public class ActivityConfigHelper implements ConfigHelper {
+
+    @PersistenceContext(unitName = "activityPUpostgres")
+    protected EntityManager em;
+
+    @Override
+    public List<String> getAllParameterKeys() {
+        List<String> allParameterKeys = new ArrayList<String>();
+        for (ParameterKey parameterKey : ParameterKey.values()) {
+            allParameterKeys.add(parameterKey.getKey());
+        }
+
+        return allParameterKeys;
+    }
+
+    @Override
+    public String getModuleName() {
+        return "Activity";
+    }
+    
+	@Override
+	public EntityManager getEntityManager() {
+		return em;
+	}
+
+}
