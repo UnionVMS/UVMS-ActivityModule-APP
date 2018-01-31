9982f89d9058bd5a40b60590e5add743aaf4b7dd
kovian <fevamko@gmail.com>
2018-01-31 11:52:06 +0100

    Refactoring

--- message/src/main/java/eu/europa/ec/fisheries/uvms/activity/message/producer/ConfigMessageProducerBean.java
+++ message/src/main/java/eu/europa/ec/fisheries/uvms/activity/message/producer/ConfigMessageProducerBean.java
@@ -12,78 +12,35 @@ copy of the GNU General Public License along with the IFDM Suite. If not, see <h
 package eu.europa.ec.fisheries.uvms.activity.message.producer;
 
 import eu.europa.ec.fisheries.uvms.commons.message.api.MessageConstants;
+import eu.europa.ec.fisheries.uvms.commons.message.api.MessageException;
+import eu.europa.ec.fisheries.uvms.commons.message.impl.AbstractProducer;
 import eu.europa.ec.fisheries.uvms.commons.message.impl.JMSUtils;
-import eu.europa.ec.fisheries.uvms.config.constants.ConfigConstants;
 import eu.europa.ec.fisheries.uvms.config.exception.ConfigMessageException;
 import eu.europa.ec.fisheries.uvms.config.message.ConfigMessageProducer;
-import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.DeliveryMode;
-import javax.jms.Destination;
-import javax.jms.JMSException;
-import javax.jms.Queue;
-import javax.jms.Session;
-import javax.jms.TextMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Stateless
-public class ConfigMessageProducerBean implements ConfigMessageProducer {
-
-    private Queue responseQueue;
-    private Queue configQueue;
-    private ConnectionFactory connectionFactory;
+public class ConfigMessageProducerBean extends AbstractProducer implements ConfigMessageProducer {
 
     final static Logger LOG = LoggerFactory.getLogger(ConfigMessageProducerBean.class);
-
-    @PostConstruct
-    public void init() {
-        connectionFactory = JMSUtils.lookupConnectionFactory();
-        configQueue = JMSUtils.lookupQueue(ConfigConstants.CONFIG_MESSAGE_IN_QUEUE);
-        responseQueue = JMSUtils.lookupQueue(MessageConstants.QUEUE_ACTIVITY);
-    }
-
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public String sendModuleMessage(String text, Queue queue) throws ConfigMessageException {
-        Connection connection = null;
-        Session session = null;
-        javax.jms.MessageProducer producer = null;
-        try {
-            connection = connectionFactory.createConnection();
-            session = JMSUtils.connectToQueue(connection);
-            TextMessage message = session.createTextMessage();
-            message.setJMSReplyTo(responseQueue);
-            message.setText(text);
-            producer = getProducer(session, queue);
-            producer.send(message);
-            return message.getJMSMessageID();
-        } catch (Exception e) {
-            LOG.error("[ Error when sending data source message. ] {}", e.getMessage());
-            throw new ConfigMessageException(e.getMessage());
-        } finally {
-            JMSUtils.disconnectQueue(connection, session, producer);
-        }
-    }
 
     @Override
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public String sendConfigMessage(String text) throws ConfigMessageException {
         try {
-            return sendModuleMessage(text, configQueue);
-        } catch (ConfigMessageException e) {
+            return sendModuleMessage(text, JMSUtils.lookupQueue(MessageConstants.QUEUE_ACTIVITY));
+        } catch (MessageException e) {
             LOG.error("[ Error when sending config message. ] {}", e.getMessage());
             throw new ConfigMessageException(e.getMessage());
         }
     }
 
-    private javax.jms.MessageProducer getProducer(Session session, Destination destination) throws JMSException {
-        javax.jms.MessageProducer producer = session.createProducer(destination);
-        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-        producer.setTimeToLive(60000L);
-        return producer;
+    @Override
+    public String getDestinationName() {
+        return MessageConstants.QUEUE_CONFIG;
     }
 }
