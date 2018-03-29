ef21503c07518448c184267a9ba9d78cef18ba8b
emrinalgr <rinaldi.gregory@gmail.com>
2018-03-29 16:56:19 +0200

    add test, refactor some mappers

---
+++ service/src/main/java/eu/europa/ec/fisheries/ers/fa/entities/QuantityType.java
@@ -0,0 +1,7 @@
+package eu.europa.ec.fisheries.ers.fa.entities;
+
+/**
+ * TODO create test
+ */
+public class FAQuantityType {
+}
---
+++ service/src/test/java/eu/europa/ec/fisheries/ers/service/mapper/VesselStorageCharacteristicsMapperTest.java
@@ -0,0 +1,52 @@
+/*
+ *
+ * Developed by the European Commission - Directorate General for Maritime Affairs and Fisheries © European Union, 2015-2016.
+ *
+ * This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version. The IFDM Suite is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package eu.europa.ec.fisheries.ers.service.mapper;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import eu.europa.ec.fisheries.ers.fa.entities.VesselStorageCharacteristicsEntity;
+import eu.europa.ec.fisheries.ers.service.util.MapperUtil;
+import org.junit.Test;
+import un.unece.uncefact.data.standard.reusableaggregatebusinessinformationentity._20.VesselStorageCharacteristic;
+
+/**
+ * Created by padhyad on 7/28/2016.
+ */
+public class VesselStorageCharactersticsMapperTest {
+
+    @Test
+    public void testDestVesselStorageCharactersticsMapper() {
+        VesselStorageCharacteristic model = MapperUtil.getVesselStorageCharacteristic();
+        VesselStorageCharacteristicsEntity vesselStorageCharacteristicsEntity = VesselStorageCharacteristicsMapper.INSTANCE.mapToDestVesselStorageCharEntity(model);
+
+        assertEquals(model.getID().getValue(), vesselStorageCharacteristicsEntity.getVesselId());
+        assertEquals(model.getID().getSchemeID(), vesselStorageCharacteristicsEntity.getVesselSchemaId());
+        assertEquals(model.getTypeCodes().get(0).getValue(), vesselStorageCharacteristicsEntity.getVesselStorageCharCode().iterator().next().getVesselTypeCode());
+        assertEquals(model.getTypeCodes().get(0).getListID(), vesselStorageCharacteristicsEntity.getVesselStorageCharCode().iterator().next().getVesselTypeCodeListId());
+        assertNull(vesselStorageCharacteristicsEntity.getFishingActivitiesForDestVesselCharId());
+    }
+
+    @Test
+    public void testSourceVesselStorageCharactersticsMapper() {
+        VesselStorageCharacteristic vesselStorageCharacteristic = MapperUtil.getVesselStorageCharacteristic();
+        VesselStorageCharacteristicsEntity vesselStorageCharacteristicsEntity = VesselStorageCharacteristicsMapper.INSTANCE.mapToDestVesselStorageCharEntity(vesselStorageCharacteristic);
+
+        assertEquals(vesselStorageCharacteristic.getID().getValue(), vesselStorageCharacteristicsEntity.getVesselId());
+        assertEquals(vesselStorageCharacteristic.getID().getSchemeID(), vesselStorageCharacteristicsEntity.getVesselSchemaId());
+        assertEquals(vesselStorageCharacteristic.getTypeCodes().get(0).getValue(), vesselStorageCharacteristicsEntity.getVesselStorageCharCode().iterator().next().getVesselTypeCode());
+        assertEquals(vesselStorageCharacteristic.getTypeCodes().get(0).getListID(), vesselStorageCharacteristicsEntity.getVesselStorageCharCode().iterator().next().getVesselTypeCodeListId());
+        assertNull(vesselStorageCharacteristicsEntity.getFishingActivitiesForSourceVesselCharId());
+    }
+}
