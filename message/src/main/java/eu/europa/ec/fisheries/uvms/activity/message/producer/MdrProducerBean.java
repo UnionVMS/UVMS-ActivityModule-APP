212107b5317153d81957e9e3855491532d010721
kovian <fevamko@yahoo.com>
2018-07-20 16:56:57 +0200

    LocalBean annotation missing

--- message/src/main/java/eu/europa/ec/fisheries/uvms/activity/message/producer/MdrProducerBean.java
+++ message/src/main/java/eu/europa/ec/fisheries/uvms/activity/message/producer/MdrProducerBean.java
@@ -1,18 +1,16 @@
 package eu.europa.ec.fisheries.uvms.activity.message.producer;
 
-import eu.europa.ec.fisheries.uvms.commons.message.impl.AbstractProducer;
 import eu.europa.ec.fisheries.uvms.commons.message.api.MessageConstants;
+import eu.europa.ec.fisheries.uvms.commons.message.impl.AbstractProducer;
 
-import javax.annotation.Resource;
-import javax.ejb.Local;
+import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
-import javax.jms.Destination;
 
 /**
  * Created by patilva on 06/04/2017.
  */
 @Stateless
-@Local
+@LocalBean
 public class MdrProducerBean extends AbstractProducer {
 
     @Override
