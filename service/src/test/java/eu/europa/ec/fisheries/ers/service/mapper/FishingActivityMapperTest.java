a0519a3ee50ab534b390ff478eff447e8995dd83
ANKOVI <fevamko@yahoo.com>
2018-10-19 16:28:59 +0200

    test failure fix

--- service/src/test/java/eu/europa/ec/fisheries/ers/service/mapper/FishingActivityMapperTest.java
+++ service/src/test/java/eu/europa/ec/fisheries/ers/service/mapper/FishingActivityMapperTest.java
@@ -47,7 +47,6 @@ public class FishingActivityMapperTest {
         assertFishingActivityFields(fishingActivity, fishingActivityEntity);
         assertNull(fishingActivityEntity.getFaReportDocument());
 
-        assertNotNull(fishingActivityEntity.getFishingGears());
         FishingGearEntity fishingGearEntity = fishingActivityEntity.getFishingGears().iterator().next();
         assertNotNull(fishingGearEntity);
         assertFishingActivityFields(fishingActivity, fishingGearEntity.getFishingActivity());
