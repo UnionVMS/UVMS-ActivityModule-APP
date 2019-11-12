01faef0cf32d36c4f206c605dbef611d23ede97f
Robert Krantz <robkra@hav.havochvatten.se>
2019-11-12 11:45:12 +0100

    Removed internal messaging for the mapToSubscriptionRequest JMS endpoint. Created a couple of tests for the endpoint

--- service/src/main/java/eu/europa/ec/fisheries/ers/activity/message/consumer/bean/ActivitySubscriptionCheckMessageConsumerBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/ers/activity/message/consumer/bean/ActivitySubscriptionCheckMessageConsumerBean.java
@@ -4,23 +4,35 @@ package eu.europa.ec.fisheries.ers.activity.message.consumer.bean;
 import eu.europa.ec.fisheries.ers.activity.message.event.ActivityMessageErrorEvent;
 import eu.europa.ec.fisheries.ers.activity.message.event.MapToSubscriptionRequestEvent;
 import eu.europa.ec.fisheries.ers.activity.message.event.carrier.EventMessage;
+import eu.europa.ec.fisheries.ers.activity.message.producer.SubscriptionProducerBean;
+import eu.europa.ec.fisheries.ers.service.mapper.SubscriptionMapper;
 import eu.europa.ec.fisheries.uvms.activity.model.exception.ActivityModelMarshallException;
 import eu.europa.ec.fisheries.uvms.activity.model.mapper.ActivityModuleResponseMapper;
 import eu.europa.ec.fisheries.uvms.activity.model.mapper.FaultCode;
 import eu.europa.ec.fisheries.uvms.activity.model.mapper.JAXBMarshaller;
 import eu.europa.ec.fisheries.uvms.activity.model.schemas.ActivityModuleMethod;
 import eu.europa.ec.fisheries.uvms.activity.model.schemas.ActivityModuleRequest;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.MapToSubscriptionRequest;
 import eu.europa.ec.fisheries.uvms.commons.message.api.MessageConstants;
 import eu.europa.ec.fisheries.uvms.commons.message.context.MappedDiagnosticContext;
+import eu.europa.ec.fisheries.uvms.commons.message.impl.JAXBUtils;
+import eu.europa.ec.fisheries.wsdl.subscription.module.SubscriptionDataRequest;
 import lombok.extern.slf4j.Slf4j;
+import un.unece.uncefact.data.standard.fluxfaquerymessage._3.FLUXFAQueryMessage;
+import un.unece.uncefact.data.standard.fluxfareportmessage._3.FLUXFAReportMessage;
 
+import javax.annotation.Resource;
 import javax.ejb.ActivationConfigProperty;
+import javax.ejb.EJB;
 import javax.ejb.MessageDriven;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
+import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
+import javax.xml.bind.JAXBException;
 
 @MessageDriven(mappedName = MessageConstants.QUEUE_MODULE_ACTIVITY, activationConfig = {
         @ActivationConfigProperty(propertyName = MessageConstants.MESSAGING_TYPE_STR, propertyValue = MessageConstants.CONNECTION_TYPE),
@@ -38,6 +50,12 @@ public class ActivitySubscriptionCheckMessageConsumerBean implements MessageList
     @Inject
     @ActivityMessageErrorEvent
     private Event<EventMessage> errorEvent;
+
+    @EJB
+    private SubscriptionProducerBean subscriptionProducer;
+
+    @Resource(mappedName =  "java:/" + MessageConstants.QUEUE_RULES)
+    private Destination rulesQueue;
 
     @Override
     public void onMessage(Message message) {
@@ -60,7 +78,7 @@ public class ActivitySubscriptionCheckMessageConsumerBean implements MessageList
             }
             switch (method) {
                 case MAP_TO_SUBSCRIPTION_REQUEST:
-                    mapToSubscriptionRequest.fire(new EventMessage(textMessage));
+                    mapToSubscriptionRequest(textMessage);
                     break;
                 default:
                     log.error("[ Request method {} is not implemented ]", method.name());
@@ -69,6 +87,30 @@ public class ActivitySubscriptionCheckMessageConsumerBean implements MessageList
 
         } catch (ActivityModelMarshallException | ClassCastException e) {
             log.error("[ Error when receiving message in activity: ] {}", e);
+            errorEvent.fire(new EventMessage(textMessage, ActivityModuleResponseMapper.createFaultMessage(FaultCode.ACTIVITY_MESSAGE, "Error when receiving message")));
+        }
+    }
+
+    private void mapToSubscriptionRequest(TextMessage textMessage) {
+        try {
+            String jmsCorrelationID = textMessage.getJMSMessageID();
+            String messageReceived = textMessage.getText();
+            SubscriptionDataRequest subscriptionDataRequest = null;
+            MapToSubscriptionRequest baseRequest = JAXBUtils.unMarshallMessage(messageReceived, MapToSubscriptionRequest.class);
+            switch (baseRequest.getMessageType()){
+                case FLUX_FA_QUERY_MESSAGE:
+                    FLUXFAQueryMessage fluxfaQueryMessage = JAXBUtils.unMarshallMessage(baseRequest.getRequest(), FLUXFAQueryMessage.class);
+                    subscriptionDataRequest = SubscriptionMapper.mapToSubscriptionDataRequest(fluxfaQueryMessage.getFAQuery());
+                    break;
+                case FLUX_FA_REPORT_MESSAGE:
+                    FLUXFAReportMessage fluxFAReportMessage = JAXBUtils.unMarshallMessage(baseRequest.getRequest(), FLUXFAReportMessage.class);
+                    subscriptionDataRequest = SubscriptionMapper.mapToSubscriptionDataRequest(fluxFAReportMessage);
+                    break;
+                default:
+                    errorEvent.fire(new EventMessage(textMessage, ActivityModuleResponseMapper.createFaultMessage(FaultCode.ACTIVITY_MESSAGE, "Exception in activity [ Forbidden fruit ]")));
+            }
+            subscriptionProducer.sendMessageWithSpecificIds(JAXBUtils.marshallJaxBObjectToString(subscriptionDataRequest), rulesQueue , null, jmsCorrelationID);
+        } catch ( JAXBException | JMSException e) {
             errorEvent.fire(new EventMessage(textMessage, ActivityModuleResponseMapper.createFaultMessage(FaultCode.ACTIVITY_MESSAGE, "Error when receiving message")));
         }
     }
