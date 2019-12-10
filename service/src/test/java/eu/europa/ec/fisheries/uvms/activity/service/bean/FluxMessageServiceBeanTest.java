47a05a8e923a1c578895c7b9fdde2054e81b8b26
Robert Krantz <robkra@hav.havochvatten.se>
2019-12-10 14:57:49 +0100

    Added test for setting start and end date on a fishing trip entity

--- service/src/test/java/eu/europa/ec/fisheries/uvms/activity/service/bean/FluxMessageServiceBeanTest.java
+++ service/src/test/java/eu/europa/ec/fisheries/uvms/activity/service/bean/FluxMessageServiceBeanTest.java
@@ -18,6 +18,8 @@ import eu.europa.ec.fisheries.schema.movement.v1.MovementType;
 import eu.europa.ec.fisheries.uvms.activity.fa.dao.FaReportDocumentDao;
 import eu.europa.ec.fisheries.uvms.activity.fa.dao.FluxFaReportMessageDao;
 import eu.europa.ec.fisheries.uvms.activity.fa.entities.FaReportDocumentEntity;
+import eu.europa.ec.fisheries.uvms.activity.fa.entities.FishingActivityEntity;
+import eu.europa.ec.fisheries.uvms.activity.fa.entities.FishingTripEntity;
 import eu.europa.ec.fisheries.uvms.activity.fa.entities.FluxFaReportMessageEntity;
 import eu.europa.ec.fisheries.uvms.activity.fa.entities.VesselIdentifierEntity;
 import eu.europa.ec.fisheries.uvms.activity.fa.utils.FaReportSourceEnum;
@@ -30,6 +32,7 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.InjectMocks;
@@ -37,22 +40,28 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.mockito.runners.MockitoJUnitRunner;
 import un.unece.uncefact.data.standard.fluxfareportmessage._3.FLUXFAReportMessage;
 import un.unece.uncefact.data.standard.reusableaggregatebusinessinformationentity._20.FAReportDocument;
+import un.unece.uncefact.data.standard.unqualifieddatatype._20.DateTimeType;
 import un.unece.uncefact.data.standard.unqualifieddatatype._20.IDType;
 
 import javax.xml.datatype.DatatypeConfigurationException;
+import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyListOf;
 
-@Ignore
+@RunWith(MockitoJUnitRunner.class)
 public class FluxMessageServiceBeanTest {
 
     @Mock
@@ -67,6 +76,9 @@ public class FluxMessageServiceBeanTest {
     @Mock
     private AssetModuleService assetModule;
 
+    @Mock
+    private FaMessageSaverBean faMessageSaverBean;
+
     @InjectMocks
     private FluxMessageServiceBean fluxMessageService;
 
@@ -74,50 +86,81 @@ public class FluxMessageServiceBeanTest {
 
     private FLUXFAReportMessage fluxFaReportMessage;
 
-    @Captor
-    ArgumentCaptor<List<FaReportDocumentEntity>> captor;
-
-    @Rule
-    public MockitoRule mockitoRule = MockitoJUnit.rule();
-
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
         FAReportDocument faReportDocument2 = MapperUtil.getFaReportDocument();
-        faReportDocument2.getRelatedFLUXReportDocument().setPurposeCode(MapperUtil.getCodeType("9", "4fyrh-58fj4-5jtu-58tjr"));
+        faReportDocument2.getRelatedFLUXReportDocument().setPurposeCode(MapperUtil.getCodeType("9", "list-id-1"));
         IDType id = faReportDocument2.getRelatedFLUXReportDocument().getReferencedID();
+        faReportDocument2.setAcceptanceDateTime(MapperUtil.getDateTimeType("2016-07-01 11:15:00"));
+        faReportDocument2.getSpecifiedFishingActivities().get(0).setOccurrenceDateTime(MapperUtil.getDateTimeType("2016-07-01 11:15:00"));
 
         FAReportDocument faReportDocument1 = MapperUtil.getFaReportDocument();
         faReportDocument1.getRelatedFLUXReportDocument().setIDS(Arrays.asList(id));
-        faReportDocument1.getRelatedFLUXReportDocument().setPurposeCode(MapperUtil.getCodeType("3", "4f5yrh-58f7j4-5j5tu-58tj7r"));
+        faReportDocument1.getRelatedFLUXReportDocument().setPurposeCode(MapperUtil.getCodeType("3", "list-id-1"));
+        faReportDocument1.setAcceptanceDateTime(MapperUtil.getDateTimeType("2016-07-01 09:15:00"));
+        faReportDocument1.getSpecifiedFishingActivities().get(0).setOccurrenceDateTime(MapperUtil.getDateTimeType("2016-07-01 09:15:00"));
 
         fluxFaReportMessage = new FLUXFAReportMessage();
         faReportDocuments = Arrays.asList(faReportDocument1, faReportDocument2);
         fluxFaReportMessage.setFAReportDocuments(faReportDocuments);
         fluxFaReportMessage.setFLUXReportDocument(MapperUtil.getFluxReportDocument());
-    }
 
-    @Test
-    @SneakyThrows
-    public void testSaveFishingActivityReportDocuments() {
-
-        //Mock the APIs
         Mockito.doReturn(new FluxFaReportMessageEntity()).when(fluxFaReportMessageDao).createEntity(Mockito.any(FluxFaReportMessageEntity.class));
 
         Mockito.doReturn(getMockedAssets()).when(assetModule).getAssetGuids(anyListOf(VesselIdentifierEntity.class));
         Mockito.doReturn(getMockedMovements()).when(movementModule).getMovement(anyListOf(String.class), Mockito.any(Instant.class), Mockito.any(Instant.class));
         Mockito.doReturn(getMockedFishingActivityReportEntity()).when(faReportDocumentDao).findFaReportByIdAndScheme(Mockito.any(String.class), Mockito.any(String.class));
+        Mockito.when(faMessageSaverBean.saveReportMessageNow(any(FluxFaReportMessageEntity.class))).then(invocation -> invocation.getArguments()[0]);
+    }
 
+    @Test
+    public void testSaveFishingActivityReportDocuments() throws Exception {
         // Trigger
         fluxMessageService.saveFishingActivityReportDocuments(fluxFaReportMessage, FaReportSourceEnum.FLUX);
 
         //Verify
-       // Mockito.verify(fluxFaReportMessageDao, Mockito.times(1)).saveFluxFaReportMessage(Mockito.any(FluxFaReportMessageEntity.class));
+        ArgumentCaptor<FluxFaReportMessageEntity> argumentCaptor = ArgumentCaptor.forClass(FluxFaReportMessageEntity.class);
+        Mockito.verify(faMessageSaverBean).saveReportMessageNow(argumentCaptor.capture());
+
+        FluxFaReportMessageEntity fluxFaReportMessageEntity = argumentCaptor.getValue();
+
         Mockito.verify(faReportDocumentDao, Mockito.times(2)).findFaReportByIdAndScheme(Mockito.any(String.class), Mockito.any(String.class));
 
         //Test
-        List<FaReportDocumentEntity>  faReportDocumentEntities = captor.getValue();
+        ArrayList<FaReportDocumentEntity> faReportDocumentEntities = new ArrayList<>(fluxFaReportMessageEntity.getFaReportDocuments());
+        FaReportDocumentEntity faReportDocumentEntity = faReportDocumentEntities.get(0);
+
         assertEquals(FaReportStatusType.getFaReportStatusEnum(Integer.parseInt(faReportDocuments.get(1).getRelatedFLUXReportDocument().getPurposeCode().getValue())).name(),
-                faReportDocumentEntities.get(0).getStatus());
+                faReportDocumentEntity.getStatus());
+    }
+
+    @Test
+    public void saveFishingReportMessage_expectFishingTripStartAndEndDateToBeSet() throws Exception {
+        // Trigger
+        fluxMessageService.saveFishingActivityReportDocuments(fluxFaReportMessage, FaReportSourceEnum.FLUX);
+
+        //Verify
+        ArgumentCaptor<FluxFaReportMessageEntity> argumentCaptor = ArgumentCaptor.forClass(FluxFaReportMessageEntity.class);
+        Mockito.verify(faMessageSaverBean).saveReportMessageNow(argumentCaptor.capture());
+
+        FluxFaReportMessageEntity fluxFaReportMessageEntity = argumentCaptor.getValue();
+
+        Mockito.verify(faReportDocumentDao, Mockito.times(2)).findFaReportByIdAndScheme(Mockito.any(String.class), Mockito.any(String.class));
+
+        //Test
+        ArrayList<FaReportDocumentEntity> faReportDocumentEntities = new ArrayList<>(fluxFaReportMessageEntity.getFaReportDocuments());
+        FaReportDocumentEntity faReportDocumentEntity = faReportDocumentEntities.get(0);
+
+        Set<FishingActivityEntity> fishingActivities = faReportDocumentEntity.getFishingActivities();
+        FishingActivityEntity fishingActivityEntity = fishingActivities.iterator().next();
+        FishingTripEntity fishingTrip = fishingActivityEntity.getFishingTrip();
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String startDate = simpleDateFormat.format(Date.from(fishingTrip.getCalculatedTripStartDate()));
+        String endDate = simpleDateFormat.format(Date.from(fishingTrip.getCalculatedTripEndDate()));
+
+        assertEquals("2016-07-01 09:15:00", startDate);
+        assertEquals("2016-07-01 11:15:00", endDate);
     }
 
     private FaReportDocumentEntity getMockedFishingActivityReportEntity() {
