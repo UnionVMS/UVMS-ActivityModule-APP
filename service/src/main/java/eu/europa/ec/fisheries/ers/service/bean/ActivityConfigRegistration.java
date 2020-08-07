ab584718363e948a8243d31b4258956538f1a60f
PAPATHEODOROU Constantinos <constantinos.papatheodorou@sword-group.com>
2020-08-07 18:47:52 +0300

    [UNIONVMS-4753] drop dependency of ActivityConfigRegistration from uvms_config PU add liquibase script for parameter table

--- service/src/main/java/eu/europa/ec/fisheries/ers/service/bean/ActivityConfigRegistration.java
+++ service/src/main/java/eu/europa/ec/fisheries/ers/service/bean/ActivityConfigRegistration.java
@@ -8,28 +8,6 @@
  details. You should have received a copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- Developed by the European Commission - Directorate General for Maritime Affairs and Fisheries @ European Union, 2015-2016.
-
- This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can redistribute it
- and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of
- the License, or any later version. The IFDM Suite is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- details. You should have received a copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
-﻿Developed with the contribution of the European Commission - Directorate General for Maritime Affairs and Fisheries
-© European Union, 2015-2016.
-
-This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can
-redistribute it and/or modify it under the terms of the GNU General Public License as published by the
-Free Software Foundation, either version 3 of the License, or any later version. The IFDM Suite is distributed in
-the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
-copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
- */
-
 package eu.europa.ec.fisheries.ers.service.bean;
 
 import javax.ejb.Stateless;
@@ -48,7 +26,7 @@ public class ActivityConfigRegistration implements ConfigHelper {
     @Inject
     private PropertiesBean propertiesBean;
 
-    @PersistenceContext(unitName = "activityConfig")
+    @PersistenceContext(unitName = "activityPUpostgres")
     private EntityManager em;
 
     @Override
@@ -70,5 +48,4 @@ public class ActivityConfigRegistration implements ConfigHelper {
     public EntityManager getEntityManager() {
         return em;
     }
-
 }
