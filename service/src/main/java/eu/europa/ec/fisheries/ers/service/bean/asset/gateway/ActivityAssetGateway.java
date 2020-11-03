f7fe550b2a626ac868178e19f24827e86d3ad651
nomikosi <42890529+nomikosi@users.noreply.github.com>
2020-11-03 23:27:51 +0200

    [UNIONVMS-4885] added JAX-RS support for asset module

---
+++ service/src/main/java/eu/europa/ec/fisheries/ers/service/bean/asset/gateway/ActivityAssetGateway.java
@@ -0,0 +1,34 @@
+/*
+ Developed by the European Commission - Directorate General for Maritime Affairs and Fisheries @ European Union, 2015-2020.
+
+ This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can redistribute it
+ and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of
+ the License, or any later version. The IFDM Suite is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ details. You should have received a copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package eu.europa.ec.fisheries.ers.service.bean.asset.gateway;
+
+
+import eu.europa.ec.fisheries.wsdl.asset.group.AssetGroup;
+import eu.europa.ec.fisheries.wsdl.asset.types.Asset;
+import eu.europa.ec.fisheries.wsdl.asset.types.AssetListQuery;
+import eu.europa.ec.fisheries.wsdl.asset.types.BatchAssetListResponseElement;
+
+import java.util.Date;
+import java.util.List;
+
+public interface ActivityAssetGateway {
+
+    List<Asset> getAssetListByQuery(AssetListQuery assetListQuery);
+
+    List<BatchAssetListResponseElement> getAssetListBatch(List<AssetListQuery> assetListQuery);
+
+    List<Asset> getAssetGroup(List<AssetGroup> assetGroupQuery);
+
+    Asset getAssetFromAssetGuidAndDate(String assetGuid, Date occurrenceDate);
+
+    Asset getAssetFromAssetHistId(String assetHistGuid);
+
+}
