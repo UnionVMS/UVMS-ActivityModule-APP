b3d2b62be124ed8b7996a29e8c8d80ad4b382426
gmanifavas <george.manifavas@sword-group.com>
2020-11-18 13:59:22 +0200

    [UNIONVMS-4866] Add missing info upon csv saving.

--- service/src/main/java/eu/europa/ec/fisheries/ers/service/bean/ActivityServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/ers/service/bean/ActivityServiceBean.java
@@ -21,7 +21,6 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Local;
 import javax.ejb.Stateless;
-import javax.mail.search.SearchException;
 import javax.transaction.Transactional;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -179,7 +178,7 @@ public class ActivityServiceBean extends BaseActivityBean implements ActivitySer
         stringBuilder.append(Optional.ofNullable(dto.getDataSource()).orElse("")).append(", ");
         stringBuilder.append(String.join(";", Optional.ofNullable(dto.getFromId()).orElse(Collections.emptyList()))).append(", ");
 
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         stringBuilder.append(Optional.ofNullable(dto.getStartDate()).map(simpleDateFormat::format).orElse("")).append(", ");  
         stringBuilder.append(Optional.ofNullable(dto.getEndDate()).map(simpleDateFormat::format).orElse("")).append(", ");
         Map<String, String> vesselIdTypes = dto.getVesselIdTypes();
