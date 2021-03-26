67535ea085048f565c922493611c5eb20c4a12de
gmanifavas <george.manifavas@sword-group.com>
2021-03-26 14:38:11 +0200

    [UNIONVMS-5096] Do not display corrected/deleted/canceled reports in catch evolution page.

--- service/src/main/java/eu/europa/ec/fisheries/ers/service/facatch/evolution/CatchProgressProcessor.java
+++ service/src/main/java/eu/europa/ec/fisheries/ers/service/facatch/evolution/CatchProgressProcessor.java
@@ -1,14 +1,18 @@
 package eu.europa.ec.fisheries.ers.service.facatch.evolution;
 
 import eu.europa.ec.fisheries.ers.fa.entities.FishingActivityEntity;
+import eu.europa.ec.fisheries.ers.fa.entities.FluxReportIdentifierEntity;
 import eu.europa.ec.fisheries.ers.fa.utils.FishingActivityTypeEnum;
 import eu.europa.ec.fisheries.ers.service.dto.fishingtrip.catchprogress.CatchProgressDTO;
 import org.apache.commons.lang3.EnumUtils;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class CatchProgressProcessor {
 
+    public static final String[] ALTERING_PURPOSE_CODES = {"1", "5", "3"};
+    
     private CatchProgressRegistry catchProgressRegistry;
 
     public CatchProgressProcessor(CatchProgressRegistry catchProgressRegistry) {
@@ -16,6 +20,7 @@ public class CatchProgressProcessor {
     }
 
     public List<CatchProgressDTO> process(List<FishingActivityEntity> fishingActivities) {
+        fishingActivities = filterAlteredReports(fishingActivities);
         fishingActivities.sort(new FishingActivityCalculatedDateComparator());
         List<CatchProgressDTO> catchProgressDTOs = new ArrayList<>();
 
@@ -35,5 +40,42 @@ public class CatchProgressProcessor {
         }
 
         return catchProgressDTOs;
+    }
+    
+    
+    
+    public List<FishingActivityEntity> filterAlteredReports(List<FishingActivityEntity> activities) {
+        List<String> alteringPurposeCodes = Arrays.asList(ALTERING_PURPOSE_CODES);
+        List<String> idsToBeRemoved = activities.stream()
+                                                .filter(fa -> alteringPurposeCodes.contains(getPurposeCode(fa)))
+                                                .map(this::getReferenceId)
+                                                .collect(Collectors.toList());
+        return activities.stream().filter(fa -> !activityReportContainsIdForRemoval(fa, idsToBeRemoved)).collect(Collectors.toList());
+    }
+    
+    public String getPurposeCode(FishingActivityEntity fishingActivityEntity) {
+        if( fishingActivityEntity.getFaReportDocument() != null && fishingActivityEntity.getFaReportDocument().getFluxReportDocument() != null) {
+            return fishingActivityEntity.getFaReportDocument().getFluxReportDocument().getPurposeCode();
+        }
+        return "";
+    }
+    
+    public String getReferenceId(FishingActivityEntity fishingActivityEntity) {
+        if( fishingActivityEntity.getFaReportDocument() != null
+                && fishingActivityEntity.getFaReportDocument().getFluxReportDocument() != null
+                && fishingActivityEntity.getFaReportDocument().getFluxReportDocument().getReferenceId() != null) {
+            return fishingActivityEntity.getFaReportDocument().getFluxReportDocument().getReferenceId();
+        }
+        return null;
+    }
+    
+    public boolean activityReportContainsIdForRemoval(FishingActivityEntity fishingActivityEntity, List<String> idsToBeRemoved) {
+        if( fishingActivityEntity.getFaReportDocument() != null
+                && fishingActivityEntity.getFaReportDocument().getFluxReportDocument() != null
+                && fishingActivityEntity.getFaReportDocument().getFluxReportDocument().getFluxReportIdentifiers() != null) {
+            String id = fishingActivityEntity.getFaReportDocument().getFluxReportDocument().getFluxReportIdentifiers().stream().findFirst().map(FluxReportIdentifierEntity::getFluxReportIdentifierId).orElse(null);
+            return idsToBeRemoved.contains(id);
+        }
+        return false;
     }
 }
